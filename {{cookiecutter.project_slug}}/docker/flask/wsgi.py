cc09ec087b19acd8b166549d3dfcc33ec3bd74e3
Emin Martinian <emin.martinian@gmail.com>
2021-12-27 18:18:07 -0500

    simplify deployment

--- {{cookiecutter.project_slug}}/docker/flask/wsgi.py
+++ {{cookiecutter.project_slug}}/docker/flask/wsgi.py
@@ -1,10 +1,10 @@
-"""WSGI file to import {{cookiecutter.github_repos.split(',')[0]}} and run it.
+"""WSGI file to import {{cookiecutter.project_slug}} and run it.
 
-This should basically only ver be run by a WSGI server.
+This should basically only be run by a WSGI server.
 """
 
 import os
-from {{ cookiecutter.github_repos.split(',')[0].split('/')[-1] }}.apps import app_factory
+from {{ cookiecutter.project_slug }}.apps import app_factory
 
 
 if __name__ == "__main__":
