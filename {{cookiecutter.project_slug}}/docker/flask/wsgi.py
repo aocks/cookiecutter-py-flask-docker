e63c412cf52282a7888e1b77870c60cca307ad6a
Emin Martinian <emin.martinian@gmail.com>
2021-12-27 20:13:44 -0500

    fix wsgi

--- {{cookiecutter.project_slug}}/docker/flask/wsgi.py
+++ {{cookiecutter.project_slug}}/docker/flask/wsgi.py
@@ -8,9 +8,9 @@ import sys
 import logging
 sys.path.append('/home/app/code')
 
-from {{ cookiecutter.project_slug }}.apps import app_factory
+from {{ cookiecutter.project_slug }}.apps.app_factory import create_app
 
 
 if __name__ == "__main__":
-    app = app_factory.create_app()
+    app = create_app()
     app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT"))
