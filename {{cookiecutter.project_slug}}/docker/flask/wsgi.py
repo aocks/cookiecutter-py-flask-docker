72ee9a67e6184909d6ed03c91909d5cb41816f8b
Emin Martinian <emin.martinian@gmail.com>
2021-12-27 19:53:06 -0500

    fix wsgi

--- {{cookiecutter.project_slug}}/docker/flask/wsgi.py
+++ {{cookiecutter.project_slug}}/docker/flask/wsgi.py
@@ -5,11 +5,16 @@ This should basically only be run by a WSGI server.
 
 import os
 import sys
+import logging
 sys.path.append('/home/app/code')
 
 from {{ cookiecutter.project_slug }}.apps import app_factory
 
 
 if __name__ == "__main__":
+    app = app_factory.create_app()
+    app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT"))
+else:
+    logging.warning('wsgi import as non-main but still creating app')
     app = app_factory.create_app()
     app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT"))
